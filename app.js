746428a7626f02dd97eb61f830e1b9dc33b49dcf
mr-l0n3lly <andreipatan@live.com>
2021-06-06 17:57:14 +0300

    added api page

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const logger = require('morgan')
 const dotenv = require('dotenv')
+const path = require('path')
 
 const productsRouter = require('./routes/productsRouter')
 
@@ -13,18 +14,23 @@ const app = express()
 app.use(logger('dev'))
 
 app.use(cors())
+app.use(express.static(path.join(__dirname, 'apidoc')))
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use('/products', productsRouter)
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'apidoc', 'index.html'))
+})
+
 app.use((err, res) => {
-	// eslint-disable-next-line no-console
-	console.log(err.stack)
-	res.status(500).json({
-		status: false,
-		message: 'Something wrong on the server',
-	})
+    // eslint-disable-next-line no-console
+    console.log(err.stack)
+    res.status(500).json({
+        status: false,
+        message: 'Something wrong on the server',
+    })
 })
 
 module.exports = app
